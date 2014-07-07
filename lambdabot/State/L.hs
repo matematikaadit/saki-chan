f3dc85117d9f4cff870ce00809a66ac6289a3b2c
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-07-07 19:13:54 +0700

    New Pristine.hs and L.hs, with some default import
    Also two new LANGUAGE pragmas.
    
    - RankNTypes
    - Rank2Types
    
    The default imports.
    
    - Control.Applicative
    - Control.Monad
    - Data.Bool
    - Data.Char
    - Data.Composition
    - Data.List
    - Data.Word
    
    [ci skip]

--- lambdabot/State/L.hs
+++ lambdabot/State/L.hs
@@ -1,5 +1,15 @@
 {-# LANGUAGE Safe #-}
 
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE Rank2Types #-}
+
 module L where
+import Control.Applicative
+import Control.Monad
+import Data.Bool
+import Data.Char
+import Data.Composition
+import Data.List
+import Data.Word
 
 {-# LINE 1 "<local>" #-}
