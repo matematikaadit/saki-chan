b7a645a1b636f34cc6a8bdc45ccebebb97b40020
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-07-10 08:01:51 +0700

    Fix some text for @gampar

--- saki-iseng-plugins/src/Saki/Plugin/Iseng/Gampar.hs
+++ saki-iseng-plugins/src/Saki/Plugin/Iseng/Gampar.hs
@@ -36,10 +36,8 @@ daftarGamparan =
     [\x -> "/me ngegampar " ++ x
     ,const "maaf, lagi nggak nerima order gampar-gamparan"
     ,const "emang loe siapa, pakai nyuruh-nyuruh segala?!"
-    ,\x -> "/me menuhin twitternya " ++ x ++ " dengan kabar gembira dari kulit manggis"
+    ,\x -> "/me menuhin twitternya " ++ x ++ " dengan kabar gembira kalau kulit manggis kini ada ekstraknya"
     ,\x -> "/me mukul " ++ x ++ " pake gayung"
-    ,\x -> "/me ngehajar " ++ x
-    ,\x -> "/me nyolok matanya " ++ x
     ,\x -> "Salah apa emang " ++ x ++ " sampai harus disakitin?"
     ,\x -> "*BRAK*, *BRUK*, *GEDUBRAK*, rasain tuh " ++ x ++ "!"
     ,const "/me ngaktifin mesin penggamparnya..."
@@ -48,24 +46,22 @@ daftarGamparan =
     ,\x -> "/me mecahin " ++ x ++ " pake batu, biar hancur berkeping-keping, biar ramai, lalu lari ke pantai"
     ,\x -> "/me ngelemparin beberapa lambdas yang tajam ke " ++ x
     ,\x -> "/me sayang " ++ x ++ ", jadi gak ada gampar-gamparan"
-    ,\x -> "/me gak akan pernah ngelukain " ++ x ++ "!"
+    ,\x -> "/me gak akan pernah ngelukain " ++ x ++ ". Titik!"
     ,\x -> "gampar sendiri gih si " ++ x ++ "!"
     ,const "Gak mau. Saya pengen kue aja."
     ,\x -> "Mending nggak deh. " ++ x ++ " kliatan ganas."
-    ,const "I don't perform such side effects on command!"
     ,const "stop nyuruh-nyuruh gue!"
     ,\x -> "/me ngeribetin " ++ x ++ " pakai bahasa imperatif"
     ,\x -> "/me nyeburin " ++ x ++ " ke kolam yang penuh buaya"
     ,\x -> "/me diam-diam ngehapus source codenya " ++ x
-    ,\x -> "/me naruh bogem mentah di rahangnya " ++ x ++ " dengan santai"
-    ,\x -> "/me ngunci si " ++ x ++ " di dalam Monad"
+    ,\x -> "/me dengan santainya naruh bogem mentah di rahangnya " ++ x
+    ,\x -> "/me ngunci si " ++ x ++ " di dalam Free Monad, trus kuncinya dibuang ke kali"
     ,\x -> "/me nyubmit alamat emailnya " ++ x ++ " ke lusinan spam lists"
     ,\x -> "/me nyetak " ++ x ++ " ke cetakan kue, lalu dimasukkan ke oven panas"
     ,const "/me bakal ngitung sampai lima..."
     ,\x -> "/me nusuk " ++ x ++ " pakai C pointer"
     ,\x -> "/me tiba-tiba dipenuhi hasrat buat nyakitin " ++ x
-    ,\x -> "Yuk, semuanya bareng-bareng mukulin " ++ x
+    ,\x -> "Yuk semuanya bareng-bareng ngegamparin " ++ x
     ,\x -> "/me ndorong " ++ x ++ " dari kursinya"
     ,\x -> "/me ngelemparin " ++ x ++ " dengan lusinan alat dapur"
-    ,\x -> "/me decomposes " ++ x ++ " into several parts using the Banach-Tarski theorem and reassembles them to get two copies of " ++ x ++ "!"
     ]
